cd1bf1f88d23fdffd7a7a3a779058c55be2a5b2e
SkyChester <dmtkachuk61@gmail.com>
2024-06-06 09:32:51 +0300

    1

--- process_registration.php
+++ process_registration.php
@@ -4,15 +4,12 @@ $username = "root";
 $password = "";
 $dbname = "registdata";
 
-// Підключення до бази даних
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Перевірка підключення
 if ($conn->connect_error) {
     die("Під'єднання не вдалося: " . $conn->connect_error);
 }
 
-// Отримання значень з POST-змінних
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -23,19 +20,16 @@ $gender = $_POST['gender'];
 $country = $_POST['country'];
 $city = $_POST['city'];
 
-// Підготовка SQL-запиту
 $sql = "INSERT INTO infouserdata (Nickname, Email, Password, Name, Surname, Birthday, Gender, Country, City) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("sssssssss", $username, $email, $password, $first_name, $last_name, $dob, $gender, $country, $city);
 
-// Виконання запиту
 if ($stmt->execute()) {
     echo "Реєстрація успішна!";
 } else {
     echo "Помилка: " . $sql . "<br>" . $conn->error;
 }
 
-// Закриття підготовленого зв'язку та підключення до бази даних
 $stmt->close();
 $conn->close();
 ?>
